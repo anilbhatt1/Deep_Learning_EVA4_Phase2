099d5988aa52532018ed830748b8da3356a53dad
Anilkumar N Bhatt <anilaknb@gmail.com>
2020-09-20 19:51:59 +0530

    S7 Modifications
    S7 Modifications

--- S7_VAE/src/losses/Train_Losses.py
+++ S7_VAE/src/losses/Train_Losses.py
@@ -2,75 +2,89 @@ from __future__ import print_function
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-import torch.optim as optim
-import numpy as np
+import torchvision.utils as vutils
 from tqdm import tqdm
-from torch.optim.lr_scheduler import ReduceLROnPlateau
+from time import time
+from datetime import datetime 
 
 # # class for Calculating and storing training losses and training accuracies of model for each batch per epoch ## 
 class Train_loss:
+
+      def get_sample_image(recontructed_img):
+          """
+          modify sample images for saving purpose
+          """
+          y_hat = recontructed_img.view(32, 3, 128, 128).permute(0, 2, 3, 1)    #Modify axes to (32, 28, 28, 3) via permute. 32 -> batch_size
+          result = (y_hat.detach().cpu().numpy()+1)/2.
+      return result
+
+      def final_loss(bce_loss, mu, logvar):
+          """
+           This function will add the reconstruction loss (BCELoss) and the KL-Divergence.
+           KL-Divergence = 0.5 * sum(1 + log(sigma^2) - mu^2 - sigma^2)
+           :param bce_loss: recontruction loss
+           :param mu: the mean from the latent vector
+           :param logvar: log variance from the latent vector
+          """
+          BCE = bce_loss 
+          KLD = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
+          return BCE + KLD     
         
-      def train_loss_calc(self,model, device, train_loader, optimizer, epoch, factor, criterion, scheduler =None, print_idx=0, maxlr=0):
+      def train_loss_calc(self,model, device, train_loader, optimizer, epoch, max_epoch, criterion):
             
           self.model        = model
           self.device       = device
           self.train_loader = train_loader
           self.optimizer    = optimizer   
           self.epoch        = epoch
-          self.factor       = factor
-          self.criterion    = criterion      
-          self.scheduler    = scheduler
-          self.print_idx    = print_idx
-          self.maxlr        = maxlr    
-          self.metric       = 0      
+          self.max_epoch    = max_epoch
+          self.criterion    = criterion
           
           model.train()
-          pbar = tqdm(train_loader)  # Wrapping train_loader in tqdm to show progress bar for each epoch while training          
-            
-          correct             = 0
-          total               = 0
-          train_losses        = []
-          train_acc           = []
-          
+          pbar = tqdm(train_loader)  # Wrapping train_loader in tqdm to show progress bar for each epoch while training         
+          img_list = []
+        
           for batch_idx, data in enumerate(pbar,0):
                         
               images, labels = data        
                        
-              images, labels = images.to(device), labels.to(device)   # Moving images and correspondig labels to GPU
-              optimizer.zero_grad()  # Zeroing out gradients at start of each batch so that backpropagation won't take accumulated value
-              labels_pred = model(images)  # Calling CNN model to predict the images
-              loss = criterion(labels_pred, labels)   # Calculating Negative Likelihood Loss by comparing prediction vs ground truth
-              
-              # Applying L1 regularization to the training loss calculated
-              L1_criterion = nn.L1Loss(size_average = None, reduce = None, reduction = 'mean')
-              reg_loss     = 0
-              for param in model.parameters():
-                zero_tensor = torch.rand_like(param) * 0 # Creating a zero tensor with same size as param
-                reg_loss    += L1_criterion(param, zero_tensor)
-              loss += factor * reg_loss 
-              
+              images      = images.to(device)       # Moving images and correspondig labels to GPU
+              optimizer.zero_grad()                 # Zeroing out gradients at start of each batch so that backpropagation won't take accumulated value
+              reconstructed_img, mu, logvar = model(images)  # Calling CNN model to predict the images
+              bce_loss    = criterion(reconstruct_img, images)
+              train_loss  = final_loss(bce_loss, mu, logvar)            
             
               # Backpropagation
-              loss.backward()
-              optimizer.step()
+              train_loss.backward()
+              optimizer.step()       
+              pbar.set_description(desc=f'Train Loss = {train_loss.item():0.6f} Epoch = {epoch} Batch Id = {batch_idx}')
+
+          if epoch % 10 == 0 or ((epoch == max_epoch-1) and (batch_idx == len(train_loader)-1)):  ### Saving reconstructed image to drive
+            print(f'Epoch: {epoch+1}/{max_epoch}, Train Loss: {train_loss.item():.6f}')
+            img = get_sample_image(recontructed_img)
+            t = datetime.now()
+            time_stamp = t.strftime("%Y")+t.strftime("%m")+t.strftime("%d")+t.strftime("%H")+t.strftime("%M")+t.strftime("%S")
+            imsave(f'/content/gdrive/My Drive/EVA4P2_S7_Training/Images_Size128_D0920/VAE_{epoch+1}_{time_stamp}.jpg', img[0])          
               
-              # This was used for OneCycle LR policy. 'get_last_lr()' giving error while using ReduceLRonPlateau hence commenting out 
-              #lr = self.scheduler.get_last_lr()[0] if self.scheduler else (self.optimizer.lr_scheduler.get_last_lr()[0] if self.optimizer.lr_scheduler else self.optimizer.param_groups[0]['lr'])
+
+          if epoch % 45 == 0 or (epoch == (max_epoch-1) and (batch_idx == len(train_loader)-1)): ### Keep the model in Gpu & Save the model values in intermittent epochs
+              t = datetime.now()
+              time_stamp = t.strftime("%Y")+t.strftime("%m")+t.strftime("%d")+t.strftime("%H")+t.strftime("%M")+t.strftime("%S")         
+              torch.save(model.state_dict(),f'/content/gdrive/My Drive/EVA4P2_S7_Training/Model_Weights_D0920/VAE_GPU_{epoch}_{time_stamp}.pt')
+              print(f'GPU model saved in epoch {epoch+1}/{max_epoch}')
               
-              lr = 0
-              if scheduler and not (isinstance(self.scheduler, torch.optim.lr_scheduler.ReduceLROnPlateau)):
-                 lr = self.scheduler.get_last_lr()[0]   # Won't work for ReduceLRonPlateau
-              else:
-                 lr = self.optimizer.param_groups[0]['lr']  
-  
-              # Calculating accuracies
-              labels_pred_max = labels_pred.argmax(dim = 1, keepdim = True) # Getting the index of max log probablity predicted by model
-              correct         += labels_pred_max.eq(labels.view_as(labels_pred_max)).sum().item() # Getting count of correctly predicted
-              total           += len(images) # Getting count of processed images
-              train_acc_batch = (correct/total)*100            
-              pbar.set_description(desc=f'Train Loss = {loss.item()} Batch Id = {batch_idx} Train Acc = {train_acc_batch:0.2f} LR ={lr:0.6f}')                                       
-     
-          train_acc.append(train_acc_batch)  # To capture only final batch accuracy of an epoch
-          train_losses.append(loss)          # To capture only final batch loss of an epoch
-        
-          return train_losses, train_acc, lr         
+              save_img = recontructed_img.detach().cpu()
+              img_list.append(vutils.make_grid(save_img, padding=2, normalize=True))  ### img_list will be used to create animation at the end of training
+
+          if (epoch == (max_epoch-1) and (batch_idx == len(train_loader)-1)):    ### Convert the model to CPU & save the model values on final epoch              
+              t = datetime.now()
+              time_stamp = t.strftime("%Y")+t.strftime("%m")+t.strftime("%d")+t.strftime("%H")+t.strftime("%M")+t.strftime("%S")
+              model.eval()
+              model.to('cpu')
+              traced_model = torch.jit.trace(model,torch.randn(1,3,128,128))      
+              traced_model.save(f'/content/gdrive/My Drive/EVA4P2_S7_Training/Model_Weights_D0920/VAE_CPU_{epoch}_{time_stamp}.pt')
+              print(f' **** CPU model Saved in epoch:{epoch+1}/{max_epoch}')
+              model.to(device)      
+              model.train()          
+              
+          return train_loss.item() , img_list  
